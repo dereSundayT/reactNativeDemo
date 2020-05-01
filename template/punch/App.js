7ed1e0209ea007e9d790f149fdac201c67fb0312
dereSundayT <sundaytoluwanidere@gmail.com>
2020-05-01 12:31:04 +0000

    Still debugging

--- template/punch/App.js
+++ template/punch/App.js
@@ -1,92 +1,102 @@
-import React ,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {StyleSheet,View,Text,TouchableOpacity, ScrollView, ListView, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ListView,
+  FlatList,
+} from 'react-native';
 import Axios from 'axios';
 
-
 const PunchUi = () => {
-    const [posts,setPosts] = useState([]);
-    //http://blog.deesuntech.com/wp-json/wp/v2/posts
-    useEffect(() => {
-             //this function will run when the page load
-            //getUserRepos(match.params.login)
-            getPosts();
-      //eslint-disable-next-line
-    },[]);
-
-     getPosts = async () =>{
-         //update loading
-         const res = await Axios.get(`http://blog.deesuntech.com/wp-json/wp/v2/posts`);
-         //update state
-         setPosts({posts:res});
-         console.log(posts);
-     }
-
-    return(
-        <View style={styles.container}>
-            <View style={styles.navBarContainer}>
-                <View style={styles.topBar}>
-                    <TouchableOpacity>
-                        <Icon name="menu" style={styles.topNavIcon}/>
-                    </TouchableOpacity>
-                    <Text style={styles.topBarTitle}>Punch News</Text>
-                    <TouchableOpacity>
-                        <Icon name="search" style={styles.topNavIcon}/>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={styles.tabItemContainer}>
-                     <Text>LATEST</Text>
-                     <Text>LATEST</Text>
-                     <Text>LATEST</Text>
-                     <Text>LATEST</Text>
-                </View>
-            </View>
-            {/* Body Section */}
-            <View>
-               <FlatList
-               keyExtractor= {(item) => item.id}
-                data={posts}
-                renderItem={(post) =>(
-                    <Text> Text </Text>
-                )}
-               />
-            </View>
-
-        </View>
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const getPosts = async () => {
+    //update loading
+    const res = await Axios.get(
+      `http://blog.deesuntech.com/wp-json/wp/v2/posts`
     );
-}
+    //update state
+    setPosts({ posts: res.data });
+    setIsLoading({ isLoading: false });
+  };
+  //http://blog.deesuntech.com/wp-json/wp/v2/posts
+  useEffect(() => {
+    //this function will run when the page load
+    //getUserRepos(match.params.login)
+    getPosts();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+      
+    <View style={styles.container}>
+      <View style={styles.navBarContainer}>
+        <View style={styles.topBar}>
+          <TouchableOpacity>
+            <Icon name='menu' style={styles.topNavIcon} />
+          </TouchableOpacity>
+          <Text style={styles.topBarTitle}>Punch News</Text>
+          <TouchableOpacity>
+            <Icon name='search' style={styles.topNavIcon} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.tabItemContainer}>
+          <Text>LATEST</Text>
+          <Text>LATEST</Text>
+          <Text>LATEST</Text>
+          <Text>LATEST</Text>
+        </View>
+      </View>
+      {/* Body Section */}
+      <View>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={posts}
+          renderItem={(post) => <Text> {post.title.rendered} </Text>}
+        />
+        <View>
+            {posts.map((post) => post.id)}
+            </View>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingTop:50,
-    },
-    navBarContainer:{
-        backgroundColor: 'red',
-        paddingHorizontal:5,
-        height: 80
-    },
-    topBar:{
-        flexDirection: 'row',
-        padding:10,
-        alignItems:"center",
-        justifyContent:"space-between",
-    },
-    topNavIcon:{
-        color:'white',
-        fontSize:25,
-    },
-    topBarTitle:{
-        color:'white',
-        fontSize:20,
-        flex:1,
-        marginLeft:10,
-    },
-    tabItemContainer:{
-        flexDirection:'row',
-        padding:10,
-        justifyContent:'space-around',
-        alignItems:'center',
-    }
-})
+  container: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  navBarContainer: {
+    backgroundColor: 'red',
+    paddingHorizontal: 5,
+    height: 80,
+  },
+  topBar: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  topNavIcon: {
+    color: 'white',
+    fontSize: 25,
+  },
+  topBarTitle: {
+    color: 'white',
+    fontSize: 20,
+    flex: 1,
+    marginLeft: 10,
+  },
+  tabItemContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
 export default PunchUi;
