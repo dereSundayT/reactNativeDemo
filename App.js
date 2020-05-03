373bbdbbe7669a77125c486e70fcdd21cfc58326
dereSundayT <sundaytoluwanidere@gmail.com>
2020-05-03 14:38:11 +0000

    setting up

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, View} from 'react-native';
-import YoutubeUi from './template/youtube/App';
-import PunchUi from './template/punch/App';
+import News from './components/NewsComponent';
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <PunchUi />
+      <News />
     </View>
   );
 }
